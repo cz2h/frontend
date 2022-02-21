b06a8eceff83ec0a1b703872f62af686eba80de3
chuhao zeng <chuhaozeng@chuhaodeMacBook-Pro.local>
2022-02-20 21:55:50 -0500

    Try to npm start with host ip configured

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 
 import Mainpage from "./layout/mainpage";
 import './App.css';
 
-
 console.log(process.env.REACT_APP_ENV);
 
 const App = (props) => {
+
   return (    
     <Mainpage/>
   );
